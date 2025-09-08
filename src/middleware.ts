6b88be384be49f90019c405e0be4710c782541ce
qarq90 <scythethroughtheheart05@gmail.com>
2025-09-08 12:46:19 +0530

    Deploy

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,8 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
     "/",
+    "/prints-queue",
+    "/orders",
     "/policies",
     "/about-us",
     "/contact-us",
