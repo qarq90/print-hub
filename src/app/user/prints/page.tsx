c16fbdb63582b36804eaa50c76649df8c24dd6d1
qarq90 <scythethroughtheheart05@gmail.com>
2025-10-19 23:44:55 +0530

    Icons - 19/10/2025

--- src/app/user/prints/page.tsx
+++ src/app/user/prints/page.tsx
@@ -5,6 +5,7 @@ import { auth, currentUser } from '@clerk/nextjs/server'
 import { UserProps } from "@/interfaces/User";
 import { SignOutButton } from "@clerk/nextjs";
 import { NotLoggedIn } from "@/components/empty/NotLoggedIn";
+import { FiCreditCard, FiLogOut } from "react-icons/fi";
 
 export const metadata: Metadata = {
     title: "Print Hub | User | Prints",
@@ -37,8 +38,13 @@ export default async function Page() {
                 <div className="flex flex-row items-center gap-4">
                     <Text size="5xl" weight="bold">Print History</Text>
                 </div>
-                <div className="cursor-pointer bg-accent text-black shadow-xs hover:bg-primary/90 px-4 py-2 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive hover:scale-105">
-                    <SignOutButton />
+                <div className="flex flex-row gap-4">
+                    <div className="cursor-pointer bg-accent text-black shadow-xs hover:bg-primary/90 px-4 py-2 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive hover:scale-105">
+                        <FiLogOut className="rotate-180 mt-0.5" /> <SignOutButton />
+                    </div>
+                    <div className="cursor-pointer bg-accent text-black shadow-xs hover:bg-primary/90 px-4 py-2 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive hover:scale-105">
+                        <FiCreditCard className="mt-0.5" /> Pay Now
+                    </div>
                 </div>
             </div>
             <Client user={userProps} />
